c21b5b2316c9a5e083abcb078ae501c8b3d8c040
forDream <gam2046@users.noreply.github.com>
2019-01-26 23:03:29 +0800

    Update command.sh

--- aria2_ftpd/command.sh
+++ aria2_ftpd/command.sh
@@ -1,8 +1,10 @@
 if [ $FTP_ENABLE ] ; then
-    mount ${FTP_USR}:${FTP_PWD}@${FTP_SRV} ${FTP_MNT}\
+    curlftpfs ${FTP_USR}:${FTP_PWD}@${FTP_SRV} ${FTP_MNT}\
     echo "Mount Remote FTP Server Finish."
 else
     echo "Disable Mount Remote FTP Server"
 fi
+
+touch /aria2.session
 
 aria2c --conf-path=/etc/aria2.conf
